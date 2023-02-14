9637297a59a4af041dea2c1b48e308049915f3db
wantoone[windows] <wantoone777@gmail.com>
2023-02-14 22:22:14 +0300

    pep8 standards are observed

--- main.py
+++ main.py
@@ -3,39 +3,38 @@
 class AbstractRepr:
 
     def get_properties(self):
-        properties = [i for i in self.__dict__ if i[:2] != ["__"] or i[0] != "_"]
-        properties_string = ""
+        properties = [i for i in self.__dict__ if i[0] != '_']
+        properties_string = ''
         for i in properties:
-            properties_string = properties_string + f"{i}: {self.__dict__[i]} "
+            properties_string = properties_string + f'{i}: {self.__dict__[i]} '
 
         return properties_string
-        
-    
+
     def __repr__(self):
-        name = str(self.__class__).split(".")[1][:-2:1]
+        name = str(self.__class__).split('.')[1][:-2:1]
         properties_string = self.get_properties()
-        return f"[{name}] {properties_string}"
-        
+        return f'[{name}] {properties_string}'
+
 
 class Person(AbstractRepr):
     def __init__(self, name, job, pay):
         self.name = name
         self.job = job
         self.pay = pay
-    
+
     def raise_pay(self, percent=0.1):
         self.pay = self.pay * (1 + percent)
-        return "Pay is raised succesfully"
-    
+        return 'Pay is raised succesfully'
+
     def __repr__(self):
         return AbstractRepr.__repr__(self)
-    
 
-if __name__ == "__main__":
-    oleg = Person("Oleg Molchanov", "dev", 20000)
-    alex = Person("Alex Romanov", "manager", 15000)
-    sue = Person("Sue Jones", "dev", 70000)
-    
+
+if __name__ == '__main__':
+    oleg = Person('Oleg Molchanov', 'dev', 20000)
+    alex = Person('Alex Romanov', 'manager', 15000)
+    sue = Person('Sue Jones', 'dev', 70000)
+
     for pers in (oleg, alex, sue):
         pers.raise_pay()
         print(pers)
